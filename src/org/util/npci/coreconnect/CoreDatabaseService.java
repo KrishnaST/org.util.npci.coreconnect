e51cd4a8e17b1361176f93a8f75b7357225a97f5
krishna.telgave <krishna.telgave@SIL_DEV076.saas.SILASPDC.com>
2019-09-21 00:52:47 +0530

    reorganized core database service.

--- src/org/util/npci/coreconnect/CoreDatabaseService.java
+++ src/org/util/npci/coreconnect/CoreDatabaseService.java
@@ -4,12 +4,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.util.datautil.Strings;
 import org.util.iso8583.ISO8583Message;
 import org.util.iso8583.ISO8583PropertyName;
 import org.util.nanolog.Logger;
 
+//@formatter:off
 public abstract class CoreDatabaseService {
 
 	protected final CoreConfig config;
@@ -101,13 +104,11 @@ public abstract class CoreDatabaseService {
 					}
 				}
 			}
-		} catch (final Exception e) {
-			logger.error(e);
-		}
+		} catch (final Exception e) {logger.error(e);}
 		return 0;
 	}
 
-	public final ISO8583Message getTransaction(final long id, final Logger logger) {
+	public final ISO8583Message getTransactionById(final long id, final Logger logger) {
 		if (isdisabled || id == 0 || Strings.isNullOrEmpty(txTableName)) return null;
 		try(final Connection con = config.dataSource.getConnection();
 				final PreparedStatement ps = con
@@ -162,12 +163,12 @@ public abstract class CoreDatabaseService {
 		return null;
 	}
 
-	public final ISO8583Message getTransaction(final String key, final Logger logger) {
-		if (isdisabled || key == null || Strings.isNullOrEmpty(txTableName)) return null;
+	public final ISO8583Message getTransactionByMKey(final String mkey, final Logger logger) {
+		if (isdisabled || mkey == null || Strings.isNullOrEmpty(txTableName)) return null;
 		try(final Connection con = config.dataSource.getConnection();
 			final PreparedStatement ps = con
 						.prepareStatement("SELECT CONVERT(VARCHAR, DECRYPTBYKEY(E002, 1, SUBSTRING(F002, (DATALENGTH(F002)-4), 4))) AS PAN, * FROM "
-								+ txTableName + " WHERE MKEY = CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', '" + key + "'), 2)");
+								+ txTableName + " WHERE MKEY = CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', '" + mkey + "'), 2)");
 			final ResultSet rs = ps.executeQuery()) {
 			if (rs.next()) {
 				final ISO8583Message transaction = new ISO8583Message();
@@ -216,41 +217,144 @@ public abstract class CoreDatabaseService {
 		}
 		return null;
 	}
+	
+	public final List<ISO8583Message> getTransactionsByTKey(final String tkey, final Logger logger) {
+		if (isdisabled || tkey == null || Strings.isNullOrEmpty(txTableName)) return null;
+		final String query = "SELECT CONVERT(VARCHAR, DECRYPTBYKEY(E002, 1, SUBSTRING(F002, (DATALENGTH(F002)-4), 4))) AS PAN, * FROM "
+				+ txTableName + " WHERE TKEY = CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', '" + tkey + "'), 2)";
+		final List<ISO8583Message> transactions = new ArrayList<ISO8583Message>();
+		try(final Connection con = config.dataSource.getConnection();
+			final PreparedStatement ps = con.prepareStatement(query);
+			final ResultSet rs = ps.executeQuery()) {
+			while (rs.next()) {
+				final ISO8583Message transaction = new ISO8583Message();
+				transaction.put(0, rs.getString("F000"));
+				transaction.put(2, encrypted ? rs.getString("PAN") : rs.getString("F002"));
+				transaction.put(3, rs.getString("F003"));
+				transaction.put(4, rs.getString("F004"));
+				transaction.put(7, rs.getString("F007"));
+				transaction.put(11, rs.getString("F011"));
+				transaction.put(12, rs.getString("F012"));
+				transaction.put(13, rs.getString("F013"));
+				transaction.put(14, rs.getString("F014"));
+				transaction.put(15, rs.getString("F015"));
+				transaction.put(16, rs.getString("F016"));
+				transaction.put(18, rs.getString("F018"));
+				transaction.put(19, rs.getString("F019"));
+				transaction.put(22, rs.getString("F022"));
+				transaction.put(23, rs.getString("F023"));
+				transaction.put(25, rs.getString("F025"));
+				transaction.put(32, rs.getString("F032"));
+				transaction.put(33, rs.getString("F033"));
+				transaction.put(37, rs.getString("F037"));
+				transaction.put(38, rs.getString("F038"));
+				transaction.put(39, rs.getString("F039"));
+				transaction.put(40, rs.getString("F040"));
+				transaction.put(41, rs.getString("F041"));
+				transaction.put(42, rs.getString("F042"));
+				transaction.put(43, rs.getString("F043"));
+				transaction.put(44, rs.getString("F044"));
+				transaction.put(48, rs.getString("F048"));
+				transaction.put(49, rs.getString("F049"));
+				transaction.put(54, rs.getString("F054"));
+				transaction.put(60, rs.getString("F060"));
+				transaction.put(61, rs.getString("F061"));
+				transaction.put(70, rs.getString("F070"));
+				transaction.put(90, rs.getString("F090"));
+				transaction.put(95, rs.getString("F095"));
+				transaction.put(102, rs.getString("F102"));
+				transaction.put(103, rs.getString("F103"));
+				transaction.put(104, rs.getString("F104"));
+				transaction.put(120, rs.getString("F120"));
+				transactions.add(transaction);
+			}
+		} catch (final Exception e) {logger.error(e);}
+		return transactions;
+	}
+	
+	public final ISO8583Message getTransactionByTKey(final String mti, final String tkey, final Logger logger) {
+		if (isdisabled || tkey == null || Strings.isNullOrEmpty(txTableName)) return null;
+		final String query = "SELECT CONVERT(VARCHAR, DECRYPTBYKEY(E002, 1, SUBSTRING(F002, (DATALENGTH(F002)-4), 4))) AS PAN, * FROM "
+				+ txTableName + " WHERE F000 = "+mti+" AND TKEY = CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', '" + tkey + "'), 2)";
+		try(final Connection con = config.dataSource.getConnection();
+			final PreparedStatement ps = con.prepareStatement(query);
+			final ResultSet rs = ps.executeQuery()) {
+			while (rs.next()) {
+				final ISO8583Message transaction = new ISO8583Message();
+				transaction.put(0, rs.getString("F000"));
+				transaction.put(2, encrypted ? rs.getString("PAN") : rs.getString("F002"));
+				transaction.put(3, rs.getString("F003"));
+				transaction.put(4, rs.getString("F004"));
+				transaction.put(7, rs.getString("F007"));
+				transaction.put(11, rs.getString("F011"));
+				transaction.put(12, rs.getString("F012"));
+				transaction.put(13, rs.getString("F013"));
+				transaction.put(14, rs.getString("F014"));
+				transaction.put(15, rs.getString("F015"));
+				transaction.put(16, rs.getString("F016"));
+				transaction.put(18, rs.getString("F018"));
+				transaction.put(19, rs.getString("F019"));
+				transaction.put(22, rs.getString("F022"));
+				transaction.put(23, rs.getString("F023"));
+				transaction.put(25, rs.getString("F025"));
+				transaction.put(32, rs.getString("F032"));
+				transaction.put(33, rs.getString("F033"));
+				transaction.put(37, rs.getString("F037"));
+				transaction.put(38, rs.getString("F038"));
+				transaction.put(39, rs.getString("F039"));
+				transaction.put(40, rs.getString("F040"));
+				transaction.put(41, rs.getString("F041"));
+				transaction.put(42, rs.getString("F042"));
+				transaction.put(43, rs.getString("F043"));
+				transaction.put(44, rs.getString("F044"));
+				transaction.put(48, rs.getString("F048"));
+				transaction.put(49, rs.getString("F049"));
+				transaction.put(54, rs.getString("F054"));
+				transaction.put(60, rs.getString("F060"));
+				transaction.put(61, rs.getString("F061"));
+				transaction.put(70, rs.getString("F070"));
+				transaction.put(90, rs.getString("F090"));
+				transaction.put(95, rs.getString("F095"));
+				transaction.put(102, rs.getString("F102"));
+				transaction.put(103, rs.getString("F103"));
+				transaction.put(104, rs.getString("F104"));
+				transaction.put(120, rs.getString("F120"));
+				return transaction;
+			}
+		} catch (final Exception e) {logger.error(e);}
+		return null;
+	}
 
-	public boolean registerResponse(final long id, final ISO8583Message response, final Logger logger) {
+	public final boolean registerResponse(final long id, final ISO8583Message response, final Logger logger) {
 		if (isdisabled || id == 0 || Strings.isNullOrEmpty(txTableName)) return false;
 		try(final Connection con = config.dataSource.getConnection();
-				final PreparedStatement ps = con.prepareStatement("UPDATE " + txTableName + " SET R039 = ?, R038 = ?, RXTIME = GETDATE() WHERE TXID = ?")) {
+			final PreparedStatement ps = con.prepareStatement("UPDATE " + txTableName + " SET R039 = ?, R038 = ?, RXTIME = GETDATE() WHERE TXID = ?")) {
 			ps.setString(1, response.get(39));
 			ps.setString(2, response.get(38));
 			ps.setLong(3, id);
 			return ps.executeUpdate() > 0;
-		} catch (final Exception e) {
-			logger.error(e);
-		}
+		} catch (final Exception e) {logger.error(e);}
 		return false;
 	}
 
 	public final boolean isDuplicateTransaction(final String key, final Logger logger) {
 		if (isdisabled || key == null || Strings.isNullOrEmpty(txTableName)) return false;
+		final String query = "SELECT CASE WHEN EXISTS (SELECT TXID FROM " + txTableName + " WITH (NOLOCK) WHERE MKEY = CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', '"
+						+ key + "'), 2)) THEN CAST (1 AS BIT)ELSE CAST (0 AS BIT) END AS EXIST";
 		try(final Connection con = config.dataSource.getConnection();
-				final PreparedStatement ps = con.prepareStatement(
-						"SELECT CASE WHEN EXISTS (SELECT TXID FROM " + txTableName + " WITH (NOLOCK) WHERE MKEY = CONVERT(VARCHAR(64), HASHBYTES('SHA2_256', '"
-								+ key + "'), 2)) THEN CAST (1 AS BIT)ELSE CAST (0 AS BIT) END AS EXIST");
+			final PreparedStatement ps = con.prepareStatement(query);
 				final ResultSet rs = ps.executeQuery()) {
 			if (rs.next()) return rs.getBoolean("EXIST");
-		} catch (final Exception e) {
-			logger.error(e);
-		}
+		} catch (final Exception e) {logger.error(e);}
 		return false;
 	}
 
-	private static final String getLast4Chars(final String string) {
+	protected static final String getLast4Chars(final String string) {
 		if (string == null || string.length() < 4) return "0000";
 		else return string.substring(string.length() - 4);
 	}
 
-	private static final String mask(final String data) {
+	protected static final String mask(final String data) {
 		if (data == null || data.length() < 11) return data;
 		return data.substring(0, 6) + String.format("%0" + (data.length() - 10) + "d", 0).replaceAll("0", "X") + data.substring(data.length() - 4);
 	}
